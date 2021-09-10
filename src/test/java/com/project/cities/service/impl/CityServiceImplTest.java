7159c714f634043d495cc8946e4ef50619db187d
miroslav <miroslav.perovic@datadesign.me>
2021-09-10 13:20:34 +0200

    Test for city service implementation

---
+++ src/test/java/com/project/cities/service/impl/CityServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.project.cities.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CityServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createCity() {
+    }
+
+    @Test
+    void getUsers() {
+    }
+}
